d27dd7cd81b985ed542fbcd7d94c562be6a2c202
sohebalam <sohebalam@hotmail.com>
2024-12-25 21:10:40 +0000

    fixes

--- lib/components/helper_func.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-void displayMessageToUser(String message, BuildContext context) {
-  showDialog(
-      context: context,
-      builder: (context) => AlertDialog(
-            title: Text(message),
-          ));
-}
